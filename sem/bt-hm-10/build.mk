ac72fc61c0e13bccd18c946dcbd7362fe4a877f0
Akash Kollipara <akashkollipara@gmail.com>
2022-04-03 21:02:20 +0530

    <SEM> Adding sources

---
+++ sem/bt-hm-10/build.mk
@@ -0,0 +1,13 @@
+#
+# CYANCORE LICENSE
+# Copyrights (C) 2022, Cyancore Team
+#
+# File Name		: build.mk
+# Description		: This file builds bluetooth hm-10 sources 
+# Primary Author	: Akash Kollipara [akashkollipara@gmail.com]
+# Organisation		: Cyancore Core-Team
+#
+
+DIR	:= $(GET_PATH)
+
+include mk/obj.mk
